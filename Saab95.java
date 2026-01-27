c91dc1ff62843902d400f59429536d3f17e8f249
fredn <fredn@student.chalmers.se>
2026-01-27 17:44:38 +0100

    Uppdaterat med det vi gjorde under labben inkl. färdigställning utav turn- och move-funktionerna
    Signed-off-by: fredn <fredn@student.chalmers.se>

--- Saab95.java
+++ Saab95.java
@@ -1,32 +1,21 @@
 import java.awt.*;
 
 public class Saab95 extends Car{
-
     private boolean turboOn;
 
     public Saab95(){
         super(125, 2, Color.red, "Saab95");
 	    turboOn = false;
     }
-
     public void setTurboOn(){
 	    turboOn = true;
     }
     public void setTurboOff(){
 	    turboOn = false;
     }
-    
     public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
-    public void incrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-    }
-    public void decrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-    }
-    
-
 }
--- Volvo240.java
+++ Volvo240.java
@@ -6,26 +6,7 @@ public class Volvo240 extends Car{
     public Volvo240(){
         super(100, 4, Color.black, "Volvo240");
     }
-    
     public double speedFactor(){
         return enginePower * 0.01 * trimFactor;
-    }
-
-    public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
-    }
-
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
-
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
     }
 }
