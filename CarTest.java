f11d9f6903335f4920e3bbbc32d965b8ab4e6796
fredn <fredn@student.chalmers.se>
2026-01-28 13:18:54 +0100

    first test
    Signed-off-by: fredn <fredn@student.chalmers.se>

--- CarTest.java
+++ CarTest.java
@@ -1,2 +1,22 @@
 public class CarTest {
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void turnLeft() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void turnRight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void gas() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void brake() {
+    }
 }
