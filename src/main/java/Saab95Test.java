467db951715a429cd37e59a6e8d0cea009e81b6d
fredn <fredn@student.chalmers.se>
2026-01-29 14:59:53 +0100

    med några tester 2

--- src/main/java/Saab95Test.java
+++ src/main/java/Saab95Test.java
@@ -26,20 +26,6 @@ class Saab95Test {
     }
 
     @Test
-    void move() {
-
-    }
-
-    @Test
-    void turnLeft() {
-
-    }
-
-    @Test
-    void turnRight() {
-    }
-
-    @Test
     void gas1() {
         saab.startEngine();
         double oldSpeed = saab.getCurrentSpeed();
@@ -47,20 +33,21 @@ class Saab95Test {
         assertEquals(oldSpeed, saab.getCurrentSpeed());
         System.out.println(saab.getCurrentSpeed());
     }
-
     @Test
     void gas2(){
         saab.startEngine();
+        double oldSpeed = saab.getCurrentSpeed();
         saab.gas(95);
-        assertEquals(0.1, saab.getCurrentSpeed());
+        assertEquals(oldSpeed, saab.getCurrentSpeed());
         System.out.println(saab.getCurrentSpeed());
     }
 
     @Test
     void gas3(){
         saab.startEngine();
+        double oldSpeed = saab.getCurrentSpeed();
         saab.gas(1);
-        assertNotEquals(0.1, saab.getCurrentSpeed(), 0.0);
+        assertNotEquals(oldSpeed, saab.getCurrentSpeed(), 0.0);
         System.out.println(saab.getCurrentSpeed());
     }
 
@@ -75,4 +62,66 @@ class Saab95Test {
         saab.brake(1);
         assertEquals(0.0, saab.getCurrentSpeed());
     }
+
+    @Test
+    void move() {
+        saab.startEngine();
+
+        double oldY = saab.orientation.getY();
+        saab.incrementSpeed(2);
+
+        saab.move();
+        assertNotEquals(oldY, saab.orientation.getY());
+
+        saab.turnLeft();
+        double oldX = saab.orientation.getX();
+        saab.move();
+        assertNotEquals(oldX, saab.orientation.getX());
+
+        saab.turnLeft();
+        oldY = saab.orientation.getY();
+        saab.move();
+        assertNotEquals(oldY, saab.orientation.getY());
+
+        saab.turnLeft();
+        oldX = saab.orientation.getX();
+        saab.move();
+        assertNotEquals(oldX, saab.orientation.getX());
+    }
+
+    @Test
+    void turnLeft() {
+        var newDirection = Orientation.Directions.WEST;
+        saab.turnLeft();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+        newDirection = Orientation.Directions.SOUTH;
+        saab.turnLeft();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+        newDirection = Orientation.Directions.EAST;
+        saab.turnLeft();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+        newDirection = Orientation.Directions.NORTH;
+        saab.turnLeft();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+    }
+
+    @Test
+    void turnRight() {
+        var newDirection = Orientation.Directions.EAST;
+        saab.turnRight();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+        newDirection = Orientation.Directions.SOUTH;
+        saab.turnRight();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+        newDirection = Orientation.Directions.WEST;
+        saab.turnRight();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+        newDirection = Orientation.Directions.NORTH;
+        saab.turnRight();
+        assertEquals(newDirection, saab.orientation.getCurrentDirection());
+    }
+
+
+
+
 }
