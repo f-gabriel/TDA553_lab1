319d2002e5c427b5f350d5f6d64107e2eb8b8d78
Anton Paulsson <antonpau@chalmers.com>
2026-02-06 11:46:58 +0100

    lagt till volvo test

---
+++ src/main/java/Scania.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Scania extends Car {
+
+    public Scania() {
+        super(500, 2, Color.red, "Scania", 0, 0, "north");
+    }
+
+
+    public void truckBed() {
+
+    }
+}
+
+
