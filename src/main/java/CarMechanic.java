808b239bb9c3a3e702845c38a8aec0b6b0ba59a9
Anders Bolin <bolinand@chalmers.se>
2026-02-10 11:03:41 +0100

    truckbedinterface

--- src/main/java/CarMechanic.java
+++ src/main/java/CarMechanic.java
@@ -1,11 +1,19 @@
 // todo: ffa fixa så att unload() följer labinstruktionerna. Kanske kräver omskrivning av load() med.
 // skapa märkesverkstäder för Saab pch Volvo
 
+import java.util.ArrayList;
+
 abstract public class CarMechanic implements Loadable<Car> {
-    Loader<Car> loader; // hjälpklass till Loadable
+
+    Loader<Car> loader = new Loader<>(15);
 
     public CarMechanic(int cargoSize) {this.loader = new Loader<>(cargoSize);}
 
     public void load(Car item){loader.load(item);}
-    public Car unLoad(){return loader.unLoad();}
+    public Car unLoad(int carNumber){
+        Car car = loader.getCargo().get(carNumber);
+        loader.getCargo().remove(car);
+        return car;
+
+    }
 }
