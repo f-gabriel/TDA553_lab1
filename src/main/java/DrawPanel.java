e85c6b2bfaf56b63f4cf589a440aa2a49eea8c20
Anton Paulsson <antonpau@chalmers.com>
2026-02-17 14:33:36 +0100

    drawpanel, kanske inte är helt rätt men kan utgå från detta iaf

--- src/main/java/DrawPanel.java
+++ src/main/java/DrawPanel.java
@@ -6,50 +6,66 @@ import javax.swing.*;
 
 // This panel represents the animated part of the view with the car images.
 
-public class DrawPanel extends JPanel{
+public class DrawPanel extends JPanel {
 
-    // Just a single image, TODO: Generalize
     BufferedImage volvoImage;
-    // To keep track of a single car's position
-    Point volvoPoint = new Point();
+    Point volvoPoint = new Point(0, 0);
+
+    BufferedImage saabImage;
+    Point saabPoint = new Point(0, 100);
+
+    BufferedImage scaniaImage;
+    Point scaniaPoint = new Point(0, 200);
 
     BufferedImage volvoWorkshopImage;
-    Point volvoWorkshopPoint = new Point(300,300);
+    Point volvoWorkshopPoint = new Point(300, 300);
 
-    // TODO: Make this general for all cars
-    void moveit(int x, int y){
-        volvoPoint.x = x;
-        volvoPoint.y = y;
+    public void moveIt(Car car, double x, double y) {
+        car.setPosition(x, y);
+        switch (car.getClass().getSimpleName()) {
+            case "Volvo240":
+                volvoPoint.x = (int) x;
+                volvoPoint.y = (int) y;
+                break;
+            case "Saab95":
+                saabPoint.x = (int) x;
+                saabPoint.y = (int) y;
+                break;
+            case "Scania":
+                scaniaPoint.x = (int) x;
+                scaniaPoint.y = (int) y;
+                break;
+            default:
+                System.out.println("Okänd biltyp: " + car.getClass().getSimpleName());
+                break;
+        }
+        repaint();
     }
 
-    // Initializes the panel and reads the images
     public DrawPanel(int x, int y) {
         this.setDoubleBuffered(true);
         this.setPreferredSize(new Dimension(x, y));
         this.setBackground(Color.green);
-        // Print an error message in case file is not found with a try/catch block
-        try {
-            // You can remove the "pics" part if running outside of IntelliJ and
-            // everything is in the same main folder.
-            // volvoImage = ImageIO.read(new File("Volvo240.jpg"));
 
-            // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
-            // if you are starting in IntelliJ.
+        try {
             volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg"));
+            saabImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Saab95.jpg"));
+            scaniaImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Scania.jpg"));
             volvoWorkshopImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg"));
-        } catch (IOException ex)
-        {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
-
     }
 
-    // This method is called each time the panel updates/refreshes/repaints itself
-    // TODO: Change to suit your needs.
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(volvoImage, volvoPoint.x, volvoPoint.y, null); // see javadoc for more info on the parameters
-        g.drawImage(volvoWorkshopImage, volvoWorkshopPoint.x, volvoWorkshopPoint.y, null);
+
+        if (volvoImage != null) g.drawImage(volvoImage, volvoPoint.x, volvoPoint.y, null);
+        if (saabImage != null) g.drawImage(saabImage, saabPoint.x, saabPoint.y, null);
+        if (scaniaImage != null) g.drawImage(scaniaImage, scaniaPoint.x, scaniaPoint.y, null);
+        if (volvoWorkshopImage != null)
+            g.drawImage(volvoWorkshopImage, volvoWorkshopPoint.x, volvoWorkshopPoint.y, null);
     }
 }
